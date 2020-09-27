e8dd72f107219e566e6f45857419d2cac5709441
Ryan T <ryantmk518@gmail.com>
2020-09-26 17:43:28 -0700

    Added jobs

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "ryantmk518@gmail.com";
+        $recipient = "info@rayzeconsulting.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
