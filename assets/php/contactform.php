c90c894e2156606d9147305b1a3e4cebf9232d32
TyeMonkey <gearbox120456@gmail.com>
2021-03-01 11:16:00 -0500

    test

--- assets/php/contactform.php
+++ assets/php/contactform.php
@@ -6,13 +6,14 @@ if (isset($_POST['submit'])){
     $subject = $_POST['subject'];
     $requests = $_POST['message'];
 
-    $mailTo = "gearbox120456@gmail.com";
+    $mailTo = "steven@pinepcs.com";
     $headers = "From: ".$mailFrom;
-    $txt = "Name: ".$name." subject: ".$subject." message: ".$requests;
+    $txt = "You have received a build request from ".$name.".\n\n"."What the customer wants to use the pc for: ".$subject.".\n"."Additional Requests ".$requests;
 
-
+    error_reporting(E_ALL);
+    ini_set('display_errors', TRUE);
+    ini_set('display_startup_errors', TRUE);
+    
     mail($mailTo, $txt, $headers);
     header("Location: index.html?mailsent");
 }
-
-?>
