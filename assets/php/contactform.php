883777b5dd46f5525862bba41c5d71ffe99d087d
TyeMonkey <gearbox120456@gmail.com>
2021-03-01 10:28:02 -0500

    form testing

---
+++ assets/php/contactform.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $subject = $_POST['subject']
+    $requests = $_POST['message'];
+
+    $mailTo = "gearbox120456@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "Name: ".$name." subject: ".$subject." message: ".$requests;
+
+
+    mail($mailTo, $txt, $headers);
+    header("Location: index.html?mailsent");
+}
